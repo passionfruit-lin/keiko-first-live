c86916fc7fa7042e18a03ac0059a6403c3dcae18
雪鷹 <dhrh89@gmail.com>
2020-08-22 20:01:03 +0800

    修改scroll magic觸發位置

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ for (var i=0; i<revealElements.length; i++) { // create a scene for each element
 	new ScrollMagic.Scene({
 						triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
 						offset: 50,												 // start a little later
-						triggerHook: 0.9,
+						triggerHook: 0.95,
 					})
 		.setClassToggle(revealElements[i], "visible") // add class toggle
 		.addTo(controller);
